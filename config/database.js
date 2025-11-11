822833b6b76f866bb5733b8e72b49866bb62a9e6
Naima e Bogran <Naima.E.Bogran@gmail.com>
2025-11-11 02:52:47 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // Replace the URL below with your own MongoDB Atlas connection string.
   // If you already have one from another project, you can reuse it.
-  url: 'mongodb+srv://naimaebogran_db_user:<db_password>@cluster3.l821bhj.mongodb.net/?appName=Cluster3'
+  url: 'mongodb+srv://naimaebogran_db_user:MyDbPass1234@cluster3.l821bhj.mongodb.net/?appName=Cluster3'
 };
